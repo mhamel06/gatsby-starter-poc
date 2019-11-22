451fb8ddde568a5e6e373b75d9cd0f4a99005a41
Matt Hamel <matt@blispay.com>
2019-11-22 10:36:56 -0500

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    plugins: [
+        '@mhamel06/gatsby-theme-events'
+    ]
+}
---
+++ src/@mhamel06/gatsby-theme-events/theme.js
@@ -0,0 +1,9 @@
+import { theme as baseTheme } from '@mhamel06/gatsby-theme-events/src/theme';
+
+export const theme = {
+    ...baseTheme,
+    colors: {
+        ...baseTheme.colors,
+        primary: 'skyblue'
+    }
+}
